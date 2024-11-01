b698faea5b441b1d7f684b12fd061183a64fefd4
LeNooby09 <phillihp.steingraeber@proton.me>
2024-11-01 13:47:32 +0100

    Window title is now current time Signed-off-by: LeNooby09 <phillihp.steingraeber@proton.me>

--- src/main.kt
+++ src/main.kt
@@ -18,17 +18,19 @@ val patterns = arrayOf(
     "[#][ ][#][#][ ][ ]", "[#][ ][#][#][ ][#]", "[#][ ][#][#][#][ ]", "[#][ ][#][#][#][#]",
     "[#][#][ ][ ][ ][ ]", "[#][#][ ][ ][ ][#]", "[#][#][ ][ ][#][ ]", "[#][#][ ][ ][#][#]",
     "[#][#][ ][#][ ][ ]", "[#][#][ ][#][ ][#]", "[#][#][ ][#][#][ ]", "[#][#][ ][#][#][#]",
-    "[#][#][#][ ][ ][ ]", "[#][#][#][ ][ ][#]", "[#][#][#][ ][#][ ]", "[#][#][#][ ][#][#]"
+    "[#][#][#][ ][ ][ ]", "[#][#][#][ ][ ][#]", "[#][#][#][ ][#][ ]", "[#][#][#][ ][#][#]",
+    "[#][#][#][#][ ][ ]", "[#][#][#][#][ ][#]", "[#][#][#][#][#][ ]", "[#][#][#][#][#][#]"
 )
 
-fun updateTextArea(textArea: JTextArea) {
+fun updateTextArea(textArea: JTextArea, frame: JFrame) {
     val now = LocalTime.now()
     textArea.text = "${patterns[now.hour]}\n${patterns[now.minute]}\n${patterns[now.second]}"
+    frame.title = now.toString().dropLast(10)
 }
 
 fun main() {
     val frame = JFrame("Binary Clock")
-    val textArea = JTextArea(3, 19)
+    val textArea = JTextArea(3, 18)
     textArea.font = java.awt.Font("Monospaced", java.awt.Font.PLAIN, 12)
     frame.add(textArea)
     frame.pack()
@@ -36,7 +38,7 @@ fun main() {
     frame.isVisible = true
 
     while (true) {
-        updateTextArea(textArea)
+        updateTextArea(textArea, frame)
         Thread.sleep(1000)
     }
 }
