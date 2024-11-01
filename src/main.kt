1d88b1a8d5d029d84c70aa77c604313cb3e9f491
LeNooby09 <phillihp.steingraeber@proton.me>
2024-11-01 14:15:34 +0100

    silly dvd effect Signed-off-by: LeNooby09 <phillihp.steingraeber@proton.me>

--- src/main.kt
+++ src/main.kt
@@ -2,6 +2,7 @@ import java.time.LocalTime
 import javax.swing.JFrame
 import javax.swing.JTextArea
 import javax.swing.WindowConstants
+import kotlin.concurrent.fixedRateTimer
 
 val patterns = arrayOf(
     "[ ][ ][ ][ ][ ][ ]", "[ ][ ][ ][ ][ ][#]", "[ ][ ][ ][ ][#][ ]", "[ ][ ][ ][ ][#][#]",
@@ -36,6 +37,29 @@ fun main() {
     frame.pack()
     frame.defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
     frame.isVisible = true
+
+    var x = 0
+    var y = 0
+    var xStep = 5
+    var yStep = 5
+
+    fixedRateTimer("timer", initialDelay = 10, period = 30) {
+        val screenSize = java.awt.Toolkit.getDefaultToolkit().screenSize
+        val frameWidth = frame.width
+        val frameHeight = frame.height
+
+        if (x + frameWidth >= screenSize.width || x < 0) {
+            xStep = -xStep
+        }
+        if (y + frameHeight >= screenSize.height || y < 0) {
+            yStep = -yStep
+        }
+
+        x += xStep
+        y += yStep
+
+        frame.setLocation(x, y)
+    }
 
     while (true) {
         updateTextArea(textArea, frame)
